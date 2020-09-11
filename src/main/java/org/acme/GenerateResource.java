1bc661318eac7223e47b206a380ea3ab1216e244
Mike Hepburn <eformat@gmail.com>
2020-09-11 20:29:37 +1000

    🐓 lowecase only 🐓

--- src/main/java/org/acme/GenerateResource.java
+++ src/main/java/org/acme/GenerateResource.java
@@ -101,8 +101,7 @@ public class GenerateResource {
 
     private String getAlphaNumericString(int n) {
         // chose a Character random from this String
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                + "0123456789"
+        String AlphaNumericString = "0123456789"
                 + "abcdefghijklmnopqrstuvxyz";
         // create StringBuffer size of AlphaNumericString
         StringBuilder sb = new StringBuilder(n);
